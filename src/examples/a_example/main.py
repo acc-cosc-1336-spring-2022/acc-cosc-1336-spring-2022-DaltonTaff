f47ff5e3a7afed7e945afc6c587b2958427eab78
DaltonTaff <dalton.tafolla@g.austincc.edu>
2022-04-09 23:49:37 -0500

    HW

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,5 +1,2 @@
-import devprocess
-
-result = devprocess.add_numbers(5, 5)
-
-print(result)
+list1 = list()
+print(list1)
--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,18 @@
-#
+def get_p_distance(list1, list2):
+    i = 0
+    di = 0
+    while i < len(list1):
+        if list1[i] != list2[i]:
+            di += 1
+        i += 1
+    p_dist = di / len(list1)
+    return p_dist
+
+def get_p_distance_matrix(list1):
+    p_distance_matrix = []
+    for r in list1:
+        row = []
+        for c in list1:
+            row.append(get_p_distance(r,c))
+        p_distance_matrix.append(row)
+    return p_distance_matrix
